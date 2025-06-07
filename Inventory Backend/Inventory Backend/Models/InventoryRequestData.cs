5dc659d8e71c33880f5f90b47dfe3c5036bcca74
GauravOj <gauravojha1111@gmail.com>
2025-06-08 00:08:56 +0530

    API CRUD Created Successfully

---
+++ Inventory Backend/Inventory Backend/Models/InventoryRequestData.cs
@@ -0,0 +1,10 @@
+﻿namespace Inventory_Backend.Models
+{
+    public class InventoryRequestData
+    {
+        public int productid { get; set; }
+        public string productname { get; set; } = string.Empty;
+        public int AvailableQuantity { get; set; }
+        public int ReOrderAmount { get; set; }
+    }
+}
